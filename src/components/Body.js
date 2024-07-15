30e1ca153083698fed3b1e068730a1f1b28bead8
Avanti <avanti.tatwawadi@gmail.com>
2024-07-15 20:31:30 +0530

    reversing some changes"

--- src/components/Body.js
+++ src/components/Body.js
@@ -23,7 +23,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "http://localhost:8010/proxy/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
 
